1eb1401035f2089d4a9cd6e8e716f02cc59e0bfe
Nathan Broadbent <nathan.f77@gmail.com>
2011-12-02 17:26:56 +0800

    Enforce XML format for ActiveResource (default changed to JSON)

--- lib/mingle4r/common_class_methods.rb
+++ lib/mingle4r/common_class_methods.rb
@@ -1,6 +1,6 @@
 module Mingle4r
   class ResourceNotSetup < Exception; end
-  
+
   module CommonClassMethods
     attr_reader :site, :user, :password
 
@@ -9,7 +9,7 @@ module Mingle4r
       # @resource_class ||= create_resource_class() # should this be commented
       @resource_class.new(args)
     end
-    
+
     # sets the site for the class in which this module is extended
     def site=(site)
       if site != self.site
@@ -39,7 +39,7 @@ module Mingle4r
       end
       @password
     end
-    
+
     # sets the collection name for the class in which this module is extended
     def collection_name=(collection_name)
       if collection_name != self.collection_name
@@ -47,7 +47,7 @@ module Mingle4r
       end
       @collection_name
     end
-    
+
     # sets the elment name for the class in which this module is extended
     def element_name=(element_name)
       if element_name != self.element_name
@@ -55,38 +55,39 @@ module Mingle4r
       end
       @element_name
     end
-    
+
     # collection name for the class in which this module is extended
     def collection_name
       @collection_name || class_name.underscore.pluralize
     end
-    
+
     # element name for the class in which this module is extended
     def element_name
       @element_name || class_name.underscore
     end
-    
+
     # def all_attributes_set?
     #   site && user && password
     # end
-    
+
     # routes to active resource find
     def find(*args)
       scope = args.slice!(0)
       options = args.slice!(0) || {}
       @resource_class.find(scope, options)
     end
-    
+
     private
     # creates an active resource class dynamically. All the attributes are set automatically. Avoid calling
-    # this method directly                                                  
+    # this method directly
     def create_resource_class
       raise "Please set the site for #{self} class." unless(self.site)
       created_class = Class.new(MingleResource)
+      created_class.format = :xml
       setup_class(created_class)
       created_class
     end
-    
+
     def setup_class(klass)
       set_resource_attributes(klass)
       set_class_name(klass)
@@ -94,21 +95,21 @@ module Mingle4r
       include_singleton_methods(klass)
       on_setup(klass) if(respond_to?('on_setup'))
     end
-    
+
     def include_singleton_methods(klass)
       klass.extend(self.const_get(:ClassMethods)) if has_class_meths_module?
     end
-    
+
     def include_instance_methods(klass)
       klass.send(:include, self.const_get(:InstanceMethods)) if has_inst_meths_module?
     end
-    
+
     def set_class_name(klass)
       created_class_name = class_name + Mingle4r::Helpers.fast_token()
       klass = self.const_set(created_class_name, klass)
       klass
     end
-    
+
     def set_resource_attributes(klass)
       klass.site = self.site
       klass.user = self.user
@@ -117,19 +118,19 @@ module Mingle4r
       klass.element_name = self.element_name
       klass
     end
-    
+
     def class_name
       self.name.demodulize
     end
-    
+
     def has_inst_meths_module?
       self.constants.detect { |const| const.to_s.demodulize == 'InstanceMethods' }
     end
-    
+
     def has_class_meths_module?
       self.constants.detect { |const| const.to_s.demodulize == 'ClassMethods' }
     end
-    
+
     def method_missing(meth_id, *args, &block)
       raise ResourceNotSetup.new("Site not set for #{name}.") unless @resource_class
       @resource_class.send(meth_id.to_sym, *args, &block)
